17ff08aa9b81dae8db8e806bb15f7807cf60ebf9
Sensei4 <dmitry.zubencko@yandex.ru>
2024-10-03 12:18:06 +0700

    Framework App and Registry Classes Created

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+if (PHP_MAJOR_VERSION < 8) {
+    die('Необходима версия PHP >= 8');
+}
+
 require_once dirname(__DIR__) . '/config/init.php';
 
-echo 'TEST';
+new \dz\App();
+
+//echo \dz\App::$app->getProperty('pagination');
+//\dz\App::$app->setProperty('test', 'TEST');
+//var_dump(\dz\App::$app->getProperties());
